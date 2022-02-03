b1ee61116a2b7cb9f871f8ed186a0339da467c3b
Matthew Malaker <MMalaker@hawk.iit.edu>
2022-02-02 18:22:45 -0600

    Initial Commit

---
+++ Shared/Homework_2_Spherical_BEssel_Functions_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework_2_Spherical_BEssel_Functions_2App.swift
+//  Shared
+//
+//  Created by Matthew Malaker on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Homework_2_Spherical_BEssel_Functions_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
